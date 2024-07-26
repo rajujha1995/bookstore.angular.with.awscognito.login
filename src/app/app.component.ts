d7096e2df6ed4868e18f459ba888ab190ef1a51d
Rajukumar Jha <raj1995.rj@gmail.com>
2024-07-26 16:11:55 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../AuthService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hello-world';
+  fullName: string | undefined;
+
+  formFields = {
+    signUp: {
+      name: {
+        order: 1,
+      },
+      email: {
+        order: 2,
+      },
+      password: {
+        order: 3,
+      },
+      confirm_password: {
+        order: 4,
+      },
+    },
+  };
+
+  constructor(private authService: AuthService) {}
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.fullName = await this.authService.getCurrentUserFullName();
+      console.log('Full Name:', this.fullName);
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+    }
+
+    let currentUser = await this.authService.getCurrentUser();
+    console.log(currentUser);
+    let currentUserSession = await this.authService.getCurrentSession();
+    console.log(currentUserSession);
+    let currentUserSessionAccessToken =
+      await this.authService.getCurrentSession();
+    console.log(currentUserSessionAccessToken?.idToken?.toString());
+  }
+
+  signOut() {
+    this.authService.signOut();
+    this.fullName = undefined;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-1_mVCScUyhH',
+      userPoolClientId: '24gpd336tlp3a2htsdvbgupd94',
+    },
+  },
+});
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AmplifyAuthenticatorModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
